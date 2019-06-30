77db98474a4afe627d7a34320fc10b8ea703f229
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 09:44:29 +0200

    Changed key-value pair format to List<Map<String, List<String>>> and simplified null check by removing unnecessary existence checks

--- src/com.engineersbox.chatmanager/AbstractFile.java
+++ src/com.engineersbox.chatmanager/AbstractFile.java
@@ -3,7 +3,9 @@ package com.engineersbox.chatmanager;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -41,26 +43,88 @@ public class AbstractFile {
         }
     }
     
-    public static void addEntry(String path, String entry) {
-    	if (config.contains(path)) {
-			List<String> currVal = config.getStringList(path);
-			currVal.add(entry);
-			config.set(path, currVal);
-    	} else {
-    		List<String> currVal = new ArrayList<String>();
-    		currVal.add(entry);
-    		config.set(path, currVal);
+    public static void addEntry(String key, String response) {
+    	String path = "Chat-Responses";
+    	if (config.get(path) != null) {
+    		
+			List<Map<?, ?>> currVal = config.getMapList(path);
+			Boolean existsFlag = false;
+			
+			for (Map<?, ?> val : currVal) {
+				if (val.containsKey(key)) {
+					@SuppressWarnings("unchecked")
+					List<String> currResponses = (List<String>) val.get(key);
+					currResponses.add(response);
+					
+					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+					newVal.put(key, currResponses);
+					
+					currVal.remove(currVal.indexOf(val));
+					currVal.add(newVal);
+					config.set(path, currVal);
+					saveConfig();
+					existsFlag = true;
+					break;
+				}
+			}
+			
+			if(existsFlag.equals(false)) {
+				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+	    		List<String> newResponse = new ArrayList<String>();
+	    		
+	    		newResponse.add(response);
+	    		newVal.put(key, newResponse);
+	    		currVal.add(newVal);
+	    		
+	    		config.set(path, currVal);
+	    		saveConfig();
+			}
     	}
     }
     
-    public static void addEntryList(String path, List<String> entry) {
-    	if (config.contains(path)) {
-			List<String> currVal = config.getStringList(path);
-			currVal.addAll(entry);
-			config.set(path, currVal);
-    	} else {
-    		config.set(path, entry);
+    public static void addEntryList(String key, List<String> response) {
+    	String path = "Chat-Responses";
+    	if (config.get(path) != null) {
+    		
+			List<Map<?, ?>> currVal = config.getMapList(path);
+			Boolean existsFlag = false;
+			
+			for (Map<?, ?> val : currVal) {
+				if (val.containsKey(key)) {
+					@SuppressWarnings("unchecked")
+					List<String> currResponses = (List<String>) val.get(key);
+					currResponses.addAll(response);
+					
+					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+					newVal.put(key, currResponses);
+					
+					currVal.remove(currVal.indexOf(val));
+					currVal.add(newVal);
+					config.set(path, currVal);
+					saveConfig();
+					existsFlag = true;
+					break;
+				}
+			}
+			
+			if(existsFlag.equals(false)) {
+				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+	    		List<String> newResponse = new ArrayList<String>();
+	    		
+	    		newResponse.addAll(response);
+	    		newVal.put(key, newResponse);
+	    		currVal.add(newVal);
+	    		
+	    		config.set(path, currVal);
+	    		saveConfig();
+			}
     	}
+    }
+    
+    public static List<List<String>> getResponses() {
+    	@SuppressWarnings("unchecked")
+		List<List<String>> list = (List<List<String>>) config.get("Chat-Responses");
+    	return list;
     }
    
 }
