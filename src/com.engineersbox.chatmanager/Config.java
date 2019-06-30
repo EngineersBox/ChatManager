89e321d47c78a48a88bd4d46421d4bf5642111a2
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 08:29:02 +0200

    Init Config()

---
+++ src/com.engineersbox.chatmanager/Config.java
@@ -0,0 +1,14 @@
+package com.engineersbox.chatmanager;
+
+import com.engineersbox.chatmanager.AbstractFile;
+import com.engineersbox.chatmanager.Main;
+
+public class Config extends AbstractFile {
+
+    public Config(Main main) {
+       
+        super(main, "config.yml");
+       
+    }
+
+}
