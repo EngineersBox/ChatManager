11cb33e8c5d0088e97d6a90b19b54747be0b2d2d
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 10:02:33 +0200

    Added command configuration for 'cm' and 'cm add', resolved improper type assignment of values

--- src/com.engineersbox.chatmanager/Commands.java
+++ src/com.engineersbox.chatmanager/Commands.java
@@ -1,5 +1,8 @@
 package com.engineersbox.chatmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,9 +13,38 @@ public class Commands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
-			//Player p = (Player) sender;
+			
+			Player p = (Player) sender;
+			
+			if (cmd.getName().equalsIgnoreCase("cm")) {
+				
+				if (args.length == 0) {
+					
+					p.sendMessage("error");
+					
+				} else if (args.length > 0) {
+					
+					if (args[0].equalsIgnoreCase("add")) {
+						
+						String key = args[1];
+						List<String> val = new ArrayList<String>();
+						
+						for (int i = 2; i < args.length; i++) {
+							val.add(args[i]);
+						}
+						
+						if (Config.addEntryList(key, val)) {
+							p.sendMessage(Main.prefix + "Keyword already exists, added to stored responses.");
+						} else {
+							p.sendMessage(Main.prefix + "Keyword does not exist, added new response");
+						}
+						return true;
+					}
+				}
+			}
+			
 		}
-		return false;
+		return true;
 	}
 
 }
