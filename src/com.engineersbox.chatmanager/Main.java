acfa334c5b2bf713dee84ce5e337e4e3878416de
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 10:01:30 +0200

    Added prefix global and command executors for 'cm' and 'cm add'

--- src/com.engineersbox.chatmanager/Main.java
+++ src/com.engineersbox.chatmanager/Main.java
@@ -3,6 +3,7 @@ package com.engineersbox.chatmanager;
 import java.io.File;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +18,9 @@ public class Main extends JavaPlugin implements Listener {
 	public static FileConfiguration config;
 	public static File cfile;
 	
+	//Globals
+	public static String prefix = ChatColor.GOLD + "[" + ChatColor.GRAY + "Chat Manager" + ChatColor.GOLD + "] ";
+	
 	public void onEnable() {
 		if (!getDataFolder().exists()) {
     		getDataFolder().mkdirs();
@@ -24,7 +28,8 @@ public class Main extends JavaPlugin implements Listener {
 		
 		new Config(this);
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		//getCommand("cm").setExecutor(new Commands());
+		getCommand("cm").setExecutor(new Commands());
+		getCommand("cm add").setExecutor(new Commands());
 	}
 	
 	@Override
