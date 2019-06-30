f54ec26a1620bf66529ab8ca21032fb6beb3d165
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 07:58:58 +0200

    Added FileConfiguration initialisation and mkdir check

---
+++ src/com.engineersbox.chatmanager/Main.java
@@ -0,0 +1,32 @@
+package com.engineersbox.chatmanager;
+
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.engineersbox.chatmanager.AbstractFile;
+import com.engineersbox.chatmanager.Commands;
+
+public class Main extends JavaPlugin implements Listener {
+
+	public static FileConfiguration config;
+	public static File cfile;
+	
+	public void onEnable() {
+		if (!getDataFolder().exists()) {
+    		getDataFolder().mkdirs();
+    	}
+		
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		getCommand("cm").setExecutor(new Commands());
+	}
+	
+	@Override
+	public void onDisable() {
+		AbstractFile.saveConfig();
+	}
+	
+}
