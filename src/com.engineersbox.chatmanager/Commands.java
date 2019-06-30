0e0069aae949c647da242c4a41dd7f99d729903f
Jack Kilrain <toastcraft.info@gmail.com>
2019-06-30 08:02:01 +0200

    Init commit for Commands.java

---
+++ src/com.engineersbox.chatmanager/Commands.java
@@ -0,0 +1,18 @@
+package com.engineersbox.chatmanager;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Commands implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player) {
+			//Player p = (Player) sender;
+		}
+		return false;
+	}
+
+}
