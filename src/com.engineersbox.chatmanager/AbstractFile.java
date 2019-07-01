69680286566ab5c1a59666815ce0af9db0e3b923
Jack Kilrain <toastcraft.info@gmail.com>
2019-07-01 11:50:45 +0200

    Move file management methods, added Player-Config default section for configuring message return

--- src/com.engineersbox.chatmanager/AbstractFile.java
+++ src/com.engineersbox.chatmanager/AbstractFile.java
@@ -2,10 +2,6 @@ package com.engineersbox.chatmanager;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -30,6 +26,7 @@ public class AbstractFile {
         AbstractFile.config = YamlConfiguration.loadConfiguration(file);
         config.addDefault("Configuration.Link-Colour", "&4");
         config.addDefault("Configuration.Link-Underline", true);
+        config.addDefault("Player-Config", null);
         config.addDefault("Chat-Responses", null);
         config.options().copyDefaults(true);
         saveConfig();
@@ -41,94 +38,6 @@ public class AbstractFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-    
-    public static boolean addEntry(String key, String response) {
-    	String path = "Chat-Responses";
-    	Boolean existsFlag = false;
-    	if (config.get(path) != null) {
-    		
-			List<Map<?, ?>> currVal = config.getMapList(path);
-			
-			for (Map<?, ?> val : currVal) {
-				if (val.containsKey(key)) {
-					@SuppressWarnings("unchecked")
-					List<String> currResponses = (List<String>) val.get(key);
-					currResponses.add(response);
-					
-					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
-					newVal.put(key, currResponses);
-					
-					currVal.remove(currVal.indexOf(val));
-					currVal.add(newVal);
-					config.set(path, currVal);
-					saveConfig();
-					existsFlag = true;
-					break;
-				}
-			}
-			
-			if(existsFlag.equals(false)) {
-				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
-	    		List<String> newResponse = new ArrayList<String>();
-	    		
-	    		newResponse.add(response);
-	    		newVal.put(key, newResponse);
-	    		currVal.add(newVal);
-	    		
-	    		config.set(path, currVal);
-	    		saveConfig();
-			}
-			
-    	}
-    	return existsFlag;
-    }
-    
-    public static boolean addEntryList(String key, List<String> response) {
-    	String path = "Chat-Responses";
-    	Boolean existsFlag = false;
-    	if (config.get(path) != null) {
-    		
-			List<Map<?, ?>> currVal = config.getMapList(path);
-			
-			for (Map<?, ?> val : currVal) {
-				if (val.containsKey(key)) {
-					@SuppressWarnings("unchecked")
-					List<String> currResponses = (List<String>) val.get(key);
-					currResponses.addAll(response);
-					
-					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
-					newVal.put(key, currResponses);
-					
-					currVal.remove(currVal.indexOf(val));
-					currVal.add(newVal);
-					config.set(path, currVal);
-					saveConfig();
-					existsFlag = true;
-					break;
-				}
-			}
-			
-			if(existsFlag.equals(false)) {
-				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
-	    		List<String> newResponse = new ArrayList<String>();
-	    		
-	    		newResponse.addAll(response);
-	    		newVal.put(key, newResponse);
-	    		currVal.add(newVal);
-	    		
-	    		config.set(path, currVal);
-	    		saveConfig();
-			}
-			
-    	}
-		return existsFlag;
-    }
-    
-    public static List<List<String>> getResponses() {
-    	@SuppressWarnings("unchecked")
-		List<List<String>> list = (List<List<String>>) config.get("Chat-Responses");
-    	return list;
     }
    
 }
