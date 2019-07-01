a529918aee0922af08dcf184bcbb29a3f4eb0487
Jack Kilrain <toastcraft.info@gmail.com>
2019-07-01 11:51:49 +0200

    moved file management methods, added getResponses(), checkPlayerConfig(), getULine() and getLinkColour()

--- src/com.engineersbox.chatmanager/Config.java
+++ src/com.engineersbox.chatmanager/Config.java
@@ -1,5 +1,12 @@
 package com.engineersbox.chatmanager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
 import com.engineersbox.chatmanager.AbstractFile;
 import com.engineersbox.chatmanager.Main;
 
@@ -9,6 +16,117 @@ public class Config extends AbstractFile {
        
         super(main, "config.yml");
        
+    }
+    
+    public static boolean addEntry(String key, String response) {
+    	String path = "Chat-Responses";
+    	Boolean existsFlag = false;
+    	if (config.get(path) != null) {
+    		
+			List<Map<?, ?>> currVal = config.getMapList(path);
+			
+			for (Map<?, ?> val : currVal) {
+				if (val.containsKey(key)) {
+					@SuppressWarnings("unchecked")
+					List<String> currResponses = (List<String>) val.get(key);
+					currResponses.add(response);
+					
+					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+					newVal.put(key, currResponses);
+					
+					currVal.remove(currVal.indexOf(val));
+					currVal.add(newVal);
+					config.set(path, currVal);
+					saveConfig();
+					existsFlag = true;
+					break;
+				}
+			}
+			
+			if(existsFlag.equals(false)) {
+				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+	    		List<String> newResponse = new ArrayList<String>();
+	    		
+	    		newResponse.add(response);
+	    		newVal.put(key, newResponse);
+	    		currVal.add(newVal);
+	    		
+	    		config.set(path, currVal);
+	    		saveConfig();
+			}
+			
+    	}
+    	return existsFlag;
+    }
+    
+    public static boolean addEntryList(String key, List<String> response) {
+    	String path = "Chat-Responses";
+    	Boolean existsFlag = false;
+    	if (config.get(path) != null) {
+    		
+			List<Map<?, ?>> currVal = config.getMapList(path);
+			
+			for (Map<?, ?> val : currVal) {
+				if (val.containsKey(key)) {
+					@SuppressWarnings("unchecked")
+					List<String> currResponses = (List<String>) val.get(key);
+					currResponses.addAll(response);
+					
+					Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+					newVal.put(key, currResponses);
+					
+					currVal.remove(currVal.indexOf(val));
+					currVal.add(newVal);
+					config.set(path, currVal);
+					saveConfig();
+					existsFlag = true;
+					break;
+				}
+			}
+			
+			if(existsFlag.equals(false)) {
+				Map<String, List<String>> newVal = new HashMap<String, List<String>>();
+	    		List<String> newResponse = new ArrayList<String>();
+	    		
+	    		newResponse.addAll(response);
+	    		newVal.put(key, newResponse);
+	    		currVal.add(newVal);
+	    		
+	    		config.set(path, currVal);
+	    		saveConfig();
+			}
+			
+    	}
+		return existsFlag;
+    }
+    
+    public static List<Map<?, ?>> getResponses() {
+		List<Map<?, ?>> list = config.getMapList("Chat-Responses");
+    	return list;
+    }
+    
+    @SuppressWarnings("unlikely-arg-type")
+	public static boolean checkPlayerConfig(Player p) {
+    	List<Boolean> players = config.getBooleanList("Player-Config");
+    	return players.contains(p.getUniqueId());
+    }
+    
+    public static String getULine() {
+    	
+    	Boolean booleanData = config.getBoolean("Configuration.Link-UnderLine");
+    	
+    	if (booleanData.equals(true)) {
+    		return "&N";
+    	} else if (booleanData.equals(false)) {
+    		return "";
+    	} else {
+    		return null;
+    	}
+    	
+    }
+    
+    public static Object getLinkColour() {
+    	return config.get("Configuration.Link-Colour");
     }
 
 }
