14b7272a7b1c26cacce4401b8316ab92b60d0e43
Jack Kilrain <toastcraft.info@gmail.com>
2019-07-04 05:01:07 +0200

    Added command config for 'cm help', 'cm version' and 'cm reload'

--- src/com.engineersbox.chatmanager/Commands.java
+++ src/com.engineersbox.chatmanager/Commands.java
@@ -1,15 +1,20 @@
 package com.engineersbox.chatmanager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import com.engineersbox.chatmanager.Main;
+
+import com.engineersbox.chatmanager.methodlib.HoverText;
 
 public class Commands implements CommandExecutor {
 
@@ -23,11 +28,57 @@ public class Commands implements CommandExecutor {
 				
 				if (args.length == 0) {
 					
-					p.sendMessage("Help Section Here");
+					String version = Bukkit.getServer().getPluginManager().getPlugin("ChatManager").getDescription().getVersion();
+        			
+    				p.sendMessage("");
+    		    	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Version Info" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+    		    	p.sendMessage("");
+    		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Version Number " + ChatColor.WHITE + ":: " + ChatColor.RED + version);
+    		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Author " + ChatColor.WHITE + ":: " + ChatColor.RED + "EngineersBox");
+    		    	p.sendMessage("");
+    		    	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Version Info" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+    		    	p.sendMessage("");
 					
 				} else if (args.length > 0) {
 					
-					if ((args[0].equalsIgnoreCase("add")) && (p.hasPermission("cm.add"))) {
+					if ((args[0].equalsIgnoreCase("version")) && (p.hasPermission("cm.version"))) {
+						
+						String version = Bukkit.getServer().getPluginManager().getPlugin("ChatManager").getDescription().getVersion();
+	        			
+	    				p.sendMessage("");
+	    		    	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Version Info" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+	    		    	p.sendMessage("");
+	    		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Version Number " + ChatColor.WHITE + ":: " + ChatColor.RED + version);
+	    		    	p.sendMessage(ChatColor.BLACK + "> " + ChatColor.DARK_GREEN + "Author " + ChatColor.WHITE + ":: " + ChatColor.RED + "EngineersBox");
+	    		    	p.sendMessage("");
+	    		    	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Version Info" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+	    		    	p.sendMessage("");
+						
+					} else if ((args[0].equalsIgnoreCase("help")) && (p.hasPermission("cm.help"))) {
+						
+		            	p.sendMessage("");
+		            	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Help" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+				    	p.sendMessage("");
+		            	HoverText.HoverMessage(p, "&0> &2/cm", Arrays.asList("&6Description:", "&cDisplays the plugin version and author"));
+						HoverText.HoverMessage(p, "&0> &2/cm add <keyword> <response> [<response>..<response>]", Arrays.asList("&6Description:", "&cAdd a keyword and response(s) to config"));
+						HoverText.HoverMessage(p, "&0> &2/cm remove <keyowrd>", Arrays.asList("&6Description:", "&cRemove a keyword and responses from config"));
+						HoverText.HoverMessage(p, "&0> &2/cm messages <enable/disable>", Arrays.asList("&6Description:", "&cConfigure visibility of messages from server"));
+						HoverText.HoverMessage(p, "&0> &2/cm reload", Arrays.asList("&6Description:", "&cReloads the plugin"));
+						HoverText.HoverMessage(p, "&0> &2/cm version", Arrays.asList("&6Description:", "&cDisplays the plugin version and author"));
+						HoverText.HoverMessage(p, "&0> &2/cm help", Arrays.asList("&6Description:", "&cOpens this menu"));
+				    	p.sendMessage("");
+				    	p.sendMessage(ChatColor.AQUA + "----={<" + ChatColor.GOLD + "  [" + ChatColor.DARK_GRAY + "ChatManager Help" + ChatColor.GOLD + "]  " + ChatColor.AQUA + "}>=----");
+				    	p.sendMessage("");
+						
+					} else if ((args[0].equalsIgnoreCase("reload")) && (p.hasPermission("cm.reload"))) {
+					
+		    			p.sendMessage(Main.prefix + ChatColor.DARK_GREEN + "Reloading ChatManager...");
+		    			Plugin plugin = p.getServer().getPluginManager().getPlugin("ChatManager");
+		    			p.getServer().getPluginManager().disablePlugin(plugin);
+		    			p.getServer().getPluginManager().enablePlugin(plugin);
+		    			p.sendMessage(Main.prefix + ChatColor.DARK_GREEN + "Reload Complete!");
+						
+					} else if ((args[0].equalsIgnoreCase("add")) && (p.hasPermission("cm.add"))) {
 						
 						String key = args[1];
 						List<String> val = new ArrayList<String>();
@@ -72,6 +123,10 @@ public class Commands implements CommandExecutor {
 						} else {
 							p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Keyword and response does not exist");
 						}
+						
+					} else if ((!p.hasPermission("cm.help")) || (!p.hasPermission("cm.version")) || (!p.hasPermission("cm.reload")) || (!p.hasPermission("cm.add")) || (!p.hasPermission("cm.msg")) || (!p.hasPermission("cm.remove"))) {
+						
+						p.sendMessage(Main.prefix + ChatColor.RED + "You Do Not Have Permission!");
 						
 					} else {
 						p.sendMessage(Main.prefix + ChatColor.DARK_PURPLE + "Invalid Command!");
